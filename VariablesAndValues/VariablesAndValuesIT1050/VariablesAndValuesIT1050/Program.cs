0d665b6902199b7cd583cae8a982d2cc474707b6
TylerVigil <97970007+TylerVigil@users.noreply.github.com>
2022-02-15 14:46:11 -0800

    Add files via upload

---
+++ VariablesAndValues/VariablesAndValuesIT1050/VariablesAndValuesIT1050/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VariablesAndValuesIT1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            int age;
+            bool likesChipotle;
+            myName = "tyler";
+            age = 20;
+            likesChipotle = false;
+        }
+    }
+}
