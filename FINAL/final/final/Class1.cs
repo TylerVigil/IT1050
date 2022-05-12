0a8716fa9869f7576cac35f3a813e05463577eae
TylerVigil <97970007+TylerVigil@users.noreply.github.com>
2022-05-12 12:22:12 -0700

    Add files via upload

---
+++ FINAL/final/final/Class1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace final
+{
+    internal class Passenger
+    {
+        private string name;
+        private double weight;
+        
+        public Passenger(string name, double weight)
+        {
+            this.name = name; 
+            this.weight = weight;
+        }
+        public string GetName()
+        {
+            return this.name;
+        }
+        public double GetWeight()
+        {
+            return this.weight;
+        }
+
+        
+    }
+}
---
+++ FINAL/final/final/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final
+{
+    internal class Elevator
+    {
+        private double maxWeight;
+        private Passenger[] Occupants;
+        
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            this.Occupants = new Passenger[maxOccupants];
+            this.maxWeight = maxWeight;
+        }
+        
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+        public double GetCurrentWeight()
+        {
+            double totalWeight = Occupants[0].GetWeight();
+            double currentOccupant;
+            for(int i = 1; i < Occupants.Length; i++)
+            {
+                currentOccupant = Occupants[i].GetWeight();
+                totalWeight = currentOccupant + totalWeight;
+                    
+            }
+            return totalWeight;
+        }
+        public bool IsOverMaxCapacity()
+        {
+            bool v = GetCurrentWeight() > maxWeight;
+            if(GetCurrentWeight() > maxWeight)
+            {
+                v = false;
+                System.Console.WriteLine("Wont Function");
+            }
+            else
+            {
+                System.Console.WriteLine("Elevator Functions");
+                v = true;
+            }
+
+            return v;
+            
+        }
+    }
+}
